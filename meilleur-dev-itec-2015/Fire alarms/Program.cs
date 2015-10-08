1a70ba907cad1479096296bf4ad9ad104ae463ca
julien-amar <julien.amar@live.fr>
2015-10-08 20:48:13 +0200

    Flooding
    Initial commit.

--- meilleur-dev-itec-2015/Fire alarms/Program.cs
+++ meilleur-dev-itec-2015/Fire alarms/Program.cs
@@ -10,38 +10,33 @@ namespace IsoGrad
 {
     class Program
     {
-
         static int GetClosestFireStation(char[][] map, int x, int y)
         {
             return -1;
         }
 
-        /// <summary>
-        /// Row 1: two space-separated integers W and L between 2 and 50 representing the width and length of the map.
-        /// Rows 2 to W + 1: L characters representing either:
-        /// - a bank 'B';
-        /// - a fire station 'F';
-        /// - an empty space '.'(dot).
-        /// To reach a bank from a fire station, there must be a path composed of empty spaces.
-        /// Row W + 2: an integer N between 1 and 20,000 representing the number of fire alarms in the city.
-        /// Rows W + 3 to W + 2 + N: two space-separated integers I and J representing the coordinates of a detected fire in a bank of the city.
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args)
         {
             var stream = Console.In;
 
+            // Row 1: two space-separated integers W and L between 2 and 50 representing the width and length of the map.
             var infos = stream.ReadLine().Split(new[] { ' ' }).Select(int.Parse).ToArray();
 
             var W = infos[0];
             var L = infos[1];
 
+            // Rows 2 to W + 1: L characters representing either:
+            // - a bank 'B';
+            // - a fire station 'F';
+            // - an empty space '.'(dot).
             var map = (from i in Enumerable.Range(0, W)
                        select stream.ReadLine().ToCharArray())
                        .ToArray();
 
+            // Row W + 2: an integer N between 1 and 20,000 representing the number of fire alarms in the city.
             var nbFireAlarm = int.Parse(stream.ReadLine());
 
+            // Rows W + 3 to W + 2 + N: two space-separated integers I and J representing the coordinates of a detected fire in a bank of the city.
             var fires = (from i in Enumerable.Range(0, nbFireAlarm)
                        select stream.ReadLine().Split(new[] { ' ' }).Select(int.Parse).ToArray())
                         .ToArray();
---
+++ meilleur-dev-itec-2015/Flooding/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsoGrad
+{
+    class Program
+    {
+        static bool[] map;
+        static int w;
+        static int l;
+        static int h;
+
+        static List<Tuple<int, int>> stack = new List<Tuple<int, int>>();
+
+        static bool GetPosition(int x, int y)
+        {
+            return map[x * l + y];
+        }
+
+        static bool Process(int x, int y)
+        {
+            stack.Add(new Tuple<int, int>(x, y));
+
+            if (x == w - 1 && y == l - 1)
+                return true;
+
+            // droite
+            if (x < w && GetPosition(x + 1, y) && !stack.Any(s => s.Item1 == x + 1 && s.Item2 == y))
+            {
+                if (Process(x + 1, y))
+                    return true;
+            }
+
+            // gauche
+            if (x > 0 && GetPosition(x - 1, y) && !stack.Any(s => s.Item1 == x - 1 && s.Item2 == y))
+            {
+                if (Process(x - 1, y))
+                    return true;
+            }
+
+            // bas
+            if (y < l && GetPosition(x, y + 1) && !stack.Any(s => s.Item1 == x && s.Item2 == y + 1))
+            {
+                if (Process(x, y + 1))
+                    return true;
+            }
+
+            // top
+            if (y > 0 && GetPosition(x, y - 1) && !stack.Any(s => s.Item1 == x && s.Item2 == y - 1))
+            {
+                if (Process(x, y - 1))
+                    return true;
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            // Row 1 : 3 integers **W**, **L** and **H** space separated representing the width and length of the building, and the height that the water has reached. **W** and **L** are between 2 and 50. **H** is between 1 and 100,000.
+            var infos = Console.ReadLine().Split(new[] { ' ' }).ToArray();
+
+            w = int.Parse(infos[0]);
+            l = int.Parse(infos[1]);
+            h = int.Parse(infos[2]);
+
+            // Row 2 to **W** + 1 : **L** integers between 1 and 100,000 separated by a space representing the height of the block for a given row. The first value of row 2 is consequently the top left corner of the building and the last value of row **W** + 1 is the bottom right corner of the building.
+            map = (from i in Enumerable.Range(0, w)
+                   from line in Console.ReadLine().Split(new[] { ' ' }).ToArray()
+                   select int.Parse(line) >= h)
+                    .ToArray();
+
+            Console.WriteLine(Process(0, 0) ? "YES" : "NO");
+        }
+    }
+}
